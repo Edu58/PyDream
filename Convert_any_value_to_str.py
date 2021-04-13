507c797d687bf56f2d163e4ec93d2e645a80eb0a
Edu58 <79555732+Edu58@users.noreply.github.com>
2021-04-13 16:45:46 +0300

    Add files via upload

---
+++ Convert_any_value_to_str.py
@@ -0,0 +1,14 @@
+# you can use str() or repr()
+
+# using str()
+cat = 978
+C = str(cat)
+print(C)
+
+
+# using repr
+# repr() is meant to generate representations which can be read by the interpreter
+# (or will force a SyntaxError if there is no equivalent syntax).
+quest = 123
+K = repr(quest)
+print(K)
